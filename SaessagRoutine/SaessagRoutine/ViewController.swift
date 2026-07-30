e751a38617258bab6d9a39a90d75f6ef90d93b67
jinseoyun1231-cyber <jin.seoyun1231@gmail.com>
2026-07-30 22:05:23 +0800

    setting :: Storyboard 제거 및 코드 기반 UI 환경 설정

---
+++ SaessagRoutine/SaessagRoutine/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SaessagRoutine
+//
+//  Created by Seoyun Jin on 7/30/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+

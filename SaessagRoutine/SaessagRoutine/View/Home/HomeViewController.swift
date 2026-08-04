2a207192c1dc5837b0e4dcc93885fe10a01bd9e0
jinseoyun1231-cyber <jin.seoyun1231@gmail.com>
2026-08-04 21:06:40 +0800

    feat :: Home 화면 Layout 구조화
    scrollView 및 주요 StackView 생성
    습관/랭킹 텍스트와 더보기 버튼 생성 및 배치
    추후 습관 카드 및 랭킹 top3 컴포넌트 스택뷰에 추가 예정

--- SaessagRoutine/SaessagRoutine/View/Home/HomeViewController.swift
+++ SaessagRoutine/SaessagRoutine/View/Home/HomeViewController.swift
@@ -11,17 +11,86 @@ import Then
 import Moya
 
 class HomeViewController: UIViewController {
-    let titleText = UILabel().then {
-        $0.text = "홈"
-        $0.font = .systemFont(ofSize: 20, weight: .bold)
+    let scrollView = UIScrollView()
+    let wholeStack = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 24
     }
+    let habitStack = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 16
+    }
+    let habitTextView = UIStackView().then {
+        $0.axis = .horizontal//가로정렬 스택
+        $0.distribution = .equalSpacing//양쪽 끝에 띄우겠다
+        $0.alignment = .center
+        $0.isLayoutMarginsRelativeArrangement = true//자체적으로 마진을 주겠냐? yes
+        $0.layoutMargins = UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 24)//좌우로 24px씩 마진 줌
+    }
+    let rankTextView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.distribution = .equalSpacing
+        $0.alignment = .center
+        $0.isLayoutMarginsRelativeArrangement = true
+        $0.layoutMargins = UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 24)
+    }
+    
+    let habitText = UILabel().then {
+        $0.text = "습관"
+        $0.font = .systemFont(ofSize: 12, weight: .regular)
+        $0.textColor = UIColor(named: "gray700")
+    }
+    let habitMoreButton = UIButton(type: .system).then {
+        let attributedString = NSMutableAttributedString(string: "더보기")
+        attributedString.addAttribute(.underlineStyle , value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: attributedString.length))
+        
+        $0.setAttributedTitle(attributedString, for: .normal)
+        $0.tintColor = UIColor(named: "gray700")
+        $0.titleLabel?.font = .systemFont(ofSize: 12, weight: .regular)
+        $0.contentHorizontalAlignment = .right
+    }
+    let rankText = UILabel().then {
+        $0.text = "랭킹"
+        $0.font = .systemFont(ofSize: 12, weight: .regular)
+        $0.textColor = UIColor(named: "gray700")
+    }
+    let rankMoreButton = UIButton(type: .system).then {
+        let attributedString = NSMutableAttributedString(string: "더보기")
+        attributedString.addAttribute(.underlineStyle , value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: attributedString.length))
+        
+        $0.setAttributedTitle(attributedString, for: .normal)
+        $0.tintColor = UIColor(named: "gray700")
+        $0.titleLabel?.font = .systemFont(ofSize: 12, weight: .regular)
+        $0.contentHorizontalAlignment = .right
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        view.addSubview(titleText)
-        titleText.snp.makeConstraints {
-            $0.center.equalToSuperview()
+        setupLayout()
+    }
+    private func setupLayout() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(wholeStack)
+        wholeStack.addArrangedSubview(habitStack)
+        wholeStack.addArrangedSubview(rankTextView)
+        
+        habitStack.addArrangedSubview(habitTextView)
+        
+        habitTextView.addArrangedSubview(habitText)
+        habitTextView.addArrangedSubview(habitMoreButton)
+        rankTextView.addArrangedSubview(rankText)
+        rankTextView.addArrangedSubview(rankMoreButton)
+        
+        scrollView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+        }
+        wholeStack.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.width.equalTo(scrollView.snp.width)
         }
     }
 }

6a8c0f595a09b89eee2b558d2210a483c8be4a2c
jinseoyun1231-cyber <jin.seoyun1231@gmail.com>
2026-08-04 16:32:37 +0800

    feat :: HabitCard 레이아웃 구성

--- SaessagRoutine/SaessagRoutine/Components/HabitCardView.swift
+++ SaessagRoutine/SaessagRoutine/Components/HabitCardView.swift
@@ -9,10 +9,119 @@ import UIKit
 import SnapKit
 import Then
 final class HabitCardView: UIView {
+    let habitCard = UIView().then { //카드
+        $0.backgroundColor = UIColor(named: "main300")
+        $0.layer.cornerRadius = 10
+    }
+    let textStack = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 2
+    }
+    let buttonStack = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 29
+    }
+    
+    let titleLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 15, weight: .medium)
+        $0.textColor = .black
+    }
+    let categoryLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 10, weight: .semibold)
+        $0.textColor = UIColor(named: "gray600")
+    }
+    let timesLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 10, weight: .semibold)
+        $0.textColor = UIColor(named: "gray600")
+    }
+    
+    let patchButton = UIButton(type: .system).then {
+        $0.setTitle("수정", for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 12, weight: .medium)
+        $0.backgroundColor = UIColor(named: "main600")
+        $0.tintColor = .white
+        $0.layer.cornerRadius = 10
+    }//수정버튼
+    let daysButton = UIButton().then {
+        $0.titleLabel?.font = .systemFont(ofSize: 12, weight: .medium)
+        $0.backgroundColor = UIColor(named: "main600")
+        $0.tintColor = .white
+        $0.layer.cornerRadius = 10
+    }//n일 성공, n주 성공
+    let verificationButton = UIButton(type: .system).then {
+        $0.setTitle("인증", for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 12, weight: .medium)
+        $0.tintColor = .white
+        $0.layer.cornerRadius = 10
+    }//인증버튼
+    
     init(titleText: String, days: Int, times: Int, didTimes: Int, category: String, cycle: String) {
         super.init(frame: .zero)
+        setAtrributes(titleText, days, times, didTimes, category, cycle)
+        setupLayout()
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setAtrributes(_ titleText: String, _ days: Int, _ times: Int, _ didTimes: Int, _ category: String, _ cycle: String) {
+        titleLabel.text = titleText
+        categoryLabel.text = category
+        timesLabel.text = "\(cycle) \(didTimes)/\(times)"
+        if cycle == "매일" {
+            daysButton.setTitle("\(days)일 성공", for: .normal)
+        } else {
+            daysButton.setTitle("\(days)주 성공", for: .normal)
+        }
+        
+        var isCompleted = (didTimes >= times)
+        if isCompleted {
+            verificationButton.isEnabled = false
+            verificationButton.backgroundColor = UIColor(named: "main500")
+        } else {
+            verificationButton.backgroundColor = UIColor(named: "main600")
+        }
+    }
+    private func setupLayout() {
+        self.addSubview(habitCard)
+        
+        habitCard.addSubview(textStack)
+        habitCard.addSubview(buttonStack)
+        
+        textStack.addArrangedSubview(titleLabel)
+        textStack.addArrangedSubview(categoryLabel)
+        textStack.addArrangedSubview(timesLabel)
+        
+        buttonStack.addArrangedSubview(verificationButton)
+        buttonStack.addArrangedSubview(patchButton)
+        buttonStack.addArrangedSubview(daysButton)
+        
+        habitCard.snp.makeConstraints {
+            $0.height.equalTo(116)
+            $0.leading.trailing.equalToSuperview().inset(24)
+        }
+        
+        verificationButton.snp.makeConstraints {
+            $0.width.equalTo(88)
+            $0.height.equalTo(24)
+        }
+        patchButton.snp.makeConstraints {
+            $0.width.equalTo(88)
+            $0.height.equalTo(24)
+        }
+        daysButton.snp.makeConstraints {
+            $0.width.equalTo(88)
+            $0.height.equalTo(24)
+        }
+        
+        textStack.snp.makeConstraints {
+            $0.leading.top.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(46)
+        }
+        buttonStack.snp.makeConstraints {
+            $0.height.equalTo(24)
+            $0.top.equalTo(textStack.snp.bottom).offset(14)
+            $0.trailing.leading.bottom.equalToSuperview().inset(16)
+        }
     }
 }

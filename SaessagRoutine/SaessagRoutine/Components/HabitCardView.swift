522881a2fd88a9c8f43cc5307e1ac9002c2115ae
jinseoyun1231-cyber <jin.seoyun1231@gmail.com>
2026-08-09 15:17:54 +0900

    feat :: 체크박스 7개 이상 시 줄바꿈 처리
    체크박스 7개 단위로 다음 줄에 배치되도록 UI 레이아웃 구현

--- SaessagRoutine/SaessagRoutine/Components/HabitCardView.swift
+++ SaessagRoutine/SaessagRoutine/Components/HabitCardView.swift
@@ -13,23 +13,24 @@ final class HabitCardView: UIView {
     var times = 0
     var didTimes = 0
     
-    let habitCard = UIView().then { //카드
+    let habitCard = UIView().then {
         $0.backgroundColor = UIColor(named: "main300")
         $0.layer.cornerRadius = 10
-    }
+    }//카드
     let textStack = UIStackView().then {
         $0.axis = .vertical
         $0.spacing = 2
-    }
+    }//텍스트 세로 스택
     let buttonStack = UIStackView().then {
         $0.axis = .horizontal
         $0.spacing = 29
-    }
-    let checkBoxStack = UIStackView().then {
-        $0.axis = .horizontal
-        $0.spacing = 24
+    }//버튼 가로 스택
+    let mainCheckBoxStack = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 20
         $0.isHidden = true
-    }
+        $0.alignment = .leading
+    }//체크박스 세로 스택
     
     let titleLabel = UILabel().then {
         $0.font = .systemFont(ofSize: 15, weight: .medium)
@@ -69,7 +70,7 @@ final class HabitCardView: UIView {
     let lineView = UIView().then {
         $0.backgroundColor = UIColor(named: "gray600")
         $0.isHidden = true
-    }
+    }//펼쳤을 때 중간에 가로 선
     
     init(titleText: String, days: Int, times: Int, didTimes: Int, category: String, cycle: String) {
         super.init(frame: .zero)
@@ -109,7 +110,7 @@ final class HabitCardView: UIView {
         habitCard.addSubview(textStack)
         habitCard.addSubview(buttonStack)
         habitCard.addSubview(lineView)
-        habitCard.addSubview(checkBoxStack)
+        habitCard.addSubview(mainCheckBoxStack)
         
         textStack.addArrangedSubview(titleLabel)
         textStack.addArrangedSubview(categoryLabel)
@@ -121,7 +122,6 @@ final class HabitCardView: UIView {
         
         
         habitCard.snp.makeConstraints {
-            $0.height.equalTo(cardHeight)
             $0.leading.trailing.equalToSuperview().inset(24)
             $0.top.bottom.equalToSuperview()
         }
@@ -146,7 +146,7 @@ final class HabitCardView: UIView {
         buttonStack.snp.makeConstraints {
             $0.height.equalTo(24)
             $0.top.equalTo(textStack.snp.bottom).offset(14)
-            $0.trailing.leading.equalToSuperview().inset(16)
+            $0.trailing.leading.bottom.equalToSuperview().inset(16)
         }
         
         lineView.snp.makeConstraints {
@@ -154,61 +154,88 @@ final class HabitCardView: UIView {
             $0.leading.trailing.equalToSuperview().inset(16)
             $0.top.equalTo(buttonStack.snp.bottom).offset(24)
         }
-        checkBoxStack.snp.makeConstraints {
+        mainCheckBoxStack.snp.makeConstraints {
             $0.leading.equalToSuperview().inset(16)
             $0.top.equalTo(lineView.snp.bottom).offset(20)
-            $0.height.equalTo(25)
+            $0.bottom.equalToSuperview().offset(-21)
         }
-    }
+    }//레이아웃 잡기
+    
     @objc func patchButtonTapped() {
-        print("수정버튼 클릭")
         onPatchButtonTapped?()//클로저 사용
     }
     @objc func verificationButtonTapped() {
-        let isExpanded = (cardHeight == 116)
+        let isExpanded = lineView.isHidden
         var checkBox : CheckBoxView
         
-        cardHeight = isExpanded ? 190 : 116
         lineView.isHidden = !isExpanded
-        checkBoxStack.isHidden = !isExpanded
+        mainCheckBoxStack.isHidden = !isExpanded
         
-        let isCompleted = (didTimes >= times)
+        let isCompleted = (didTimes >= times)//인증 성공 여부 판단
         if isCompleted {
             verificationButton.isEnabled = false
             verificationButton.backgroundColor = UIColor(named: "main500")
             habitCard.backgroundColor = UIColor(named: "main300")
-        } else {
-            verificationButton.backgroundColor = UIColor(named: "main600")
-            habitCard.backgroundColor = UIColor(named: "gray300")
-        }
-
+        }//성공 했으면 인증버튼 비활성화, 배경색 변경
+        
         if isExpanded {
-            checkBoxStack.arrangedSubviews.forEach{
+            mainCheckBoxStack.snp.remakeConstraints {
+                $0.leading.equalToSuperview().inset(16)
+                $0.top.equalTo(lineView.snp.bottom).offset(20)
+                $0.bottom.equalToSuperview().offset(-21)
+            }//카드 밑에를 체크박스에서 21만큼 떨어지게
+            buttonStack.snp.remakeConstraints {
+                $0.height.equalTo(24)
+                $0.top.equalTo(textStack.snp.bottom).offset(14)
+                $0.trailing.leading.equalToSuperview().inset(16)
+            }//카드 바텀이 버튼에서 16 떨어졌던거 없앰
+            mainCheckBoxStack.arrangedSubviews.forEach{//원래 있던 박스들 삭제
                 $0.removeFromSuperview()
             }
+
+            var checkBoxHStack: UIStackView?//가로 스택 생성
             for i in 0 ..< self.times {
+                if i % 7 == 0 {//
+                    checkBoxHStack = UIStackView().then {
+                        $0.axis = .horizontal
+                        $0.spacing = 24
+                        $0.alignment = .center
+                        $0.distribution = .fill
+                        mainCheckBoxStack.addArrangedSubview($0)
+                    }//가로 줄 추가
+                }//7개 단위로 가로 줄 하나씩 추가 하는.
                 if i < self.didTimes {
-                    checkBox = CheckBoxView(isChecked: true)
+                    checkBox = CheckBoxView(isChecked: true)//체크 돼 있는걸로 생성
                 } else {
-                    checkBox = CheckBoxView(isChecked: false)
-                }
-                checkBoxStack.addArrangedSubview(checkBox)
+                    checkBox = CheckBoxView(isChecked: false)// 안 돼있는걸로 생성
+                }//체크박스 생성
                 checkBox.onChecked = { [weak self] isNowChecked in
                     guard let self = self else { return }
 
-                    if isNowChecked {
+                    if isNowChecked {//체크 됐을 때
                         self.didTimes += 1
-                    } else {
+                    } else {//체크 돼있던 애가 취소 됐을 때
                         self.didTimes -= 1
                     }
-                    let cycleText = self.timesLabel.text?.components(separatedBy: " ").first ?? ""
-                    self.timesLabel.text = "\(cycleText) \(self.didTimes)/\(self.times)"
-                }
+                    let cycleText = self.timesLabel.text?.components(separatedBy: " ").first ?? ""//매일, 월요일 같은 텍스트 추출
+                    self.timesLabel.text = "\(cycleText) \(self.didTimes)/\(self.times)"//"매일 6/8" 텍스트 변경
+                }//체크 됐을 때 텍스트 변경 하는 클로저
+                checkBoxHStack?.addArrangedSubview(checkBox)//가로 체크박스 스택에 박스 하나하나 추가
+            }//체크박스 생성
+        }//확장 됐을 때 할 액션
+        else {
+            mainCheckBoxStack.snp.removeConstraints()
+            buttonStack.snp.remakeConstraints {
+                $0.height.equalTo(24)
+                $0.top.equalTo(textStack.snp.bottom).offset(14)
+                $0.trailing.leading.equalToSuperview().inset(16)
+                $0.bottom.equalToSuperview().inset(16)
             }
+
         }
         
-        habitCard.snp.updateConstraints {
-            $0.height.equalTo(cardHeight)
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut) {
+            self.superview?.layoutIfNeeded()
         }
     }
 }

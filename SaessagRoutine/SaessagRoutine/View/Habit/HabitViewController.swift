9c80d5909b3c02674bdf01be11cf3114c60a2fe6
jinseoyun1231-cyber <jin.seoyun1231@gmail.com>
2026-08-13 23:15:32 +0900

    feat :: 습관 생성 화면 이동 버튼 추가
    습관 메인 화면에 습관 생성 이동 버튼 추가 및 버튼 연동
    습관 수정 화면에 네비게이션 바 추가

--- SaessagRoutine/SaessagRoutine/View/Habit/HabitViewController.swift
+++ SaessagRoutine/SaessagRoutine/View/Habit/HabitViewController.swift
@@ -16,11 +16,23 @@ class HabitViewController: UIViewController {
         $0.text = "Habit"
         $0.font = .systemFont(ofSize: 20, weight: .bold)
     }
+    let createButton = UIButton(type: .system).then {
+        $0.imageView?.contentMode = .scaleAspectFit
+        $0.setImage(UIImage(systemName: "plus"), for: .normal)
+        $0.tintColor = UIColor(named: "main800")
+        $0.addTarget(self, action: #selector(plusTapped), for: .touchUpInside)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        setLayout()
+    }
+    private func setLayout() {
         view.addSubview(titleText)
         view.addSubview(topBar)
+        view.addSubview(createButton)
+        
         titleText.snp.makeConstraints {
             $0.center.equalToSuperview()
         }
@@ -28,6 +40,14 @@ class HabitViewController: UIViewController {
             $0.top.leading.trailing.equalToSuperview()
             $0.height.equalTo(101)
         }
-    }
+        createButton.snp.makeConstraints {
+            $0.top.equalTo(topBar.snp.bottom)
+            $0.trailing.equalToSuperview().inset(24)
+            $0.height.width.equalTo(25)
+        }
+    }//레이아웃 잡기
+    @objc private func plusTapped() {
+        navigationController?.pushViewController(HabitCreateViewController(), animated: false)
+    }//생성 버튼 클릭 시 습관 생성 화면으로 이동
 }
 
